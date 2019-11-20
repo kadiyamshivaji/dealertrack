1f4c5193eea369015a99b67fc3453ecaa5fca5a8
kadiyamshivaji@gmail.com <kadiyamshivaji@gmail.com>
2019-11-20 20:30:00 +0530

    added fromik

--- dealer/src/App.js
+++ dealer/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import HomeContainer from './containers/HomeContainer'
+import { Formik } from 'formik';
 function App() {
   return (
-    <div className="App">
+    <div className="App">   
       <header className="App-header">
         <HomeContainer></HomeContainer>
       </header>
