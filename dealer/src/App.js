c70bed338685cc311e50959f00ba23571182a3b5
kadiyamshivaji@gmail.com <kadiyamshivaji@gmail.com>
2019-11-21 10:30:04 +0530

    space

--- dealer/src/App.js
+++ dealer/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">   
       <header className="App-header">
-        <HomeContainer></HomeContainer>
+        <HomeContainer> </HomeContainer>
       </header>
     </div>
   );
