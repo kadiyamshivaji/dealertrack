923205d98d528b9124683a12abba18ec096ffa59
kadiyamshivaji@gmail.com <kadiyamshivaji@gmail.com>
2019-11-21 12:28:44 +0530

    formik basic form

--- dealer/src/App.js
+++ dealer/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import HomeContainer from './containers/HomeContainer'
+import HomeContainer from './containers/HomeContainer';
+import FormComponent from './Form';
 import { Formik } from 'formik';
 function App() {
   return (
     <div className="App">   
       <header className="App-header">
-        <HomeContainer> </HomeContainer>
+        <FormComponent> </FormComponent>
       </header>
     </div>
   );
--- dealer/src/Form.js
+++ dealer/src/Form.js
@@ -1,147 +1,103 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Form, Icon, Input, Button, Checkbox ,Row,Col,Select,Divider} from 'antd';
-import {setNextPage} from '../actions';
-import 'antd/dist/antd.css';
+import "./helper.css";
+import { MoreResources, DisplayFormikState } from "./helper";
 
-const ButtonGroup = Button.Group;
-const { Option } = Select;
-  class PersonalContainer extends React.Component {
+import React from "react";
+import { render } from "react-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-
-    currentPage(e){
-      this.props.setNextPage({payload:e})
-    }
-
-    nextClick = e => {
-      e.preventDefault();
-      this.props.setNextPage({payload:4})
-    };
+  class FormComponent extends React.Component {
     render() {
-      const { getFieldDecorator } = this.props.form;
-      // const {firstname,lastname,phone,email,ssn,date,firstnameJ,lastnameJ,
-      //         phoneJ,ssnJ,dateJ} =this.props.contactInfo;
-        // const {own,ownJ,rent,rentJ,address,addressJ,
-        //         city,cityJ,state,stateJ,zipcode,zipcodeJ,}=this.props.housingInfo;
-        // const {employeed,employeedJ,employer,employerJ,money,moneyJ,paytype,paytypeJ}=this.props.employementInfo;
       return (
-        <div>
-        <h2 className="h3">Review and Submit </h2>
-        <i>Please take one last chance to everything .Make changes if needed.</i>
-        <div>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-                 <Row gutter={12}>
-                        <Col span={12}>
-                            <label>Primary Applicant
-                            <Icon type="edit" style={{ color: 'blue' }} onClick={(e) => this.currentPage(0)}  />
-                            </label>    <br/><br/>
-                            <p>{(this.props.contactInfo||{}).firstname||{}} {(this.props.contactInfo||{}).lastname||{}}</p>
-                            <p>{(this.props.contactInfo||{}).phone||{}} </p>
-                            <p>{(this.props.contactInfo||{}).email||{}} </p>
-                            {/* <p>{(this.props.contactInfo||{}).date||{}} </p> */}
-                            <p>{(this.props.contactInfo||{}).ssn||{}} </p>
-                        </Col>
-                        {this.props.selectedSection==='Joint' && 
-
-                            <Col span={12}>
-                            <label>Secondary Applicant
-                                <Icon type="edit" style={{ color: 'blue' }} onClick={(e) => this.currentPage(0)} />
-                                </label> <br/><br/>
-                                <p>{(this.props.contactInfo||{}).firstnameJ||{}} {(this.props.contactInfo||{}).lastnameJ||{}}</p>
-                                <p>{(this.props.contactInfo||{}).phoneJ||{}} </p>
-                            <p>{(this.props.contactInfo||{}).email||{}} </p>
-                            {/* <p>{(this.props.contactInfo||{}).dateJ||{}} </p> */}
-                            <p>{(this.props.contactInfo||{}).ssnJ||{}} </p>
-                            </Col>  
-                        }
-                  </Row>
-                 <Row gutter={12}>
-                        <Col span={12}>
-                            <label>Housing
-                            <Icon type="edit" style={{ color: 'blue' }} onClick={(e) => this.currentPage(1)} />
-                            </label><br/><br/>
-                            <p>{(this.props.housingInfo||{}).own||{}} </p>
-                            <p>{(this.props.housingInfo||{}).rent||{}} </p>
-                            <p>{(this.props.housingInfo||{}).address||{}} </p>
-                            <p>{(this.props.housingInfo||{}).city||{}},{(this.props.housingInfo||{}).state||{}},{(this.props.housingInfo||{}).zipcode||{}} </p>
-                            <p>{(this.props.housingInfo||{}).suitNo||{}} </p>
-                            <p>Over 2 years</p>
-                            <p>Spouse</p>
-                        </Col>
-                        {this.props.selectedSection==='Joint' &&                        
-            
-                            <Col span={12}>
-                            <label>Housing
-                                <Icon type="edit" style={{ color: 'blue' }} onClick={(e) => this.currentPage(1)} />
-                                </label> 
-                                <br/><br/>
-                                <p>{(this.props.housingInfo||{}).ownJ||{}} </p>
-                                <p>{(this.props.housingInfo||{}).rentJ||{}} </p>
-                                <p>{(this.props.housingInfo||{}).addressJ||{}} </p>
-                                <p>{(this.props.housingInfo||{}).cityJ||{}},{(this.props.housingInfo||{}).stateJ||{}},{(this.props.housingInfo||{}).zipcodeJ||{}} </p>
-                                <p>{(this.props.housingInfo||{}).suitNoJ||{}} </p>
-                                <p>Over 2 years</p>
-                                <p>Spouse</p>
-                            </Col>
-                     }
-                  </Row>
-                   <Row gutter={12}>
-                        <Col span={12}>
-                            <label>Employment
-                            <Icon type="edit" style={{ color: 'blue' }} onClick={(e) => this.currentPage(2)} />
-                            </label><br/><br/>
-                               <p>{(this.props.employementInfo||{}).employeed||{}} </p>
-                               <p>{(this.props.employementInfo||{}).employer||{}} </p>
-                               <p>{(this.props.employementInfo||{}).money||{}}/{(this.props.employementInfo||{}).paytype||{}} </p>
-                        </Col>
-                        {this.props.selectedSection==='Joint' && 
-                        <Col span={12}>
-                            <label>Employment
-                            <Icon type="edit" style={{ color: 'blue' }} onClick={(e) => this.currentPage(2)} />
-                            </label> <br/><br/>
-                            <p>{(this.props.employementInfo||{}).employeedJ||{}} </p>
-                               <p>{(this.props.employementInfo||{}).employerJ||{}} </p>
-                               <p>{(this.props.employementInfo||{}).moneyJ||{}}/{(this.props.employementInfo||{}).paytypeJ||{}} </p>
-                     
-                        </Col>
-                        }
-                  </Row>  
-                  <Form.Item>
-                      {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                      })(<Checkbox>I have reviewed and agree to the <a>Terms & Conditions</a> and <a>Privacy Policy</a>.
-                      I understand  this dealer will check my credit and share the ressults with their lender</Checkbox>)}
-                    </Form.Item>
-                  { this.props.selectedSection==='Joint' && 
-                  <Form.Item>
-                      {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                      })(<Checkbox>Both borrowers inttend to apply for joint credit.</Checkbox>)}
-                    </Form.Item>
-              }
-            <Divider/>
-                  <Form.Item >
-                    <Button   type="primary" htmlType="submit" className="login-form-button">
-                         Submit Application
-                    </Button>
-                 </Form.Item>   
-        </Form>
-          </div>
-        </div>
+        <div className="app">
+        <h1>
+          Basic{" "}
+          <a
+            href="https://github.com/jaredpalmer/formik"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Formik
+          </a>{" "}
+          Demo
+        </h1>
+    
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={async values => {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email()
+              .required("Required")
+          })}
+        >
+          {props => {
+            const {
+              values,
+              touched,
+              errors,
+              dirty,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              handleReset
+            } = props;
+            return (
+              <form className="container" onSubmit={handleSubmit}>
+                <div className="form-group">       
+                  <input name="email" type="text" 
+                    className={`form-control ${errors.email && touched.email && 'is-invalid'}`}
+                    value={values.email} 
+                    onChange={handleChange}
+                    onBlur={handleBlur} />
+                  {errors.email && touched.email && <div className="invalid-feedback">{errors.email}</div>}
+                </div>
+                <div className="form-group">
+                    <input
+                      id="email"
+                      placeholder="Email"
+                      type="text"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={
+                        errors.email && touched.email
+                          ? "text-input error"
+                          : "text-input"
+                      }
+                    />
+                {errors.email && touched.email && (
+                  <div className="input-feedback">{errors.email}</div>
+                )}
+     </div>
+                <button
+                  type="button"
+                  className="outline"
+                  onClick={handleReset}
+                  disabled={!dirty || isSubmitting}
+                >
+                  Reset
+                </button>
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+    
+                <DisplayFormikState {...props} />
+              </form>
+            );
+          }}
+        </Formik>
+    
+        <MoreResources />
+      </div>
+     
+     
       );
     }
   }
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(PersonalContainer);
-const mapStateToProps = (state) => ({
-  selectedSection:state.contactSection,
-  contactInfo:state.contactInfo,
-  housingInfo:state.housingInfo,
-  employementInfo:state.employmentInfo
-})
-const mapDispatchToProps = {
-  setNextPage:setNextPage
-};
-PersonalContainer = connect(mapStateToProps, mapDispatchToProps)(WrappedNormalLoginForm);
-export default PersonalContainer;
+
+export default FormComponent;
